58623862707dd3de163103ca04c2271abbbee07e
[rcastelo] <[robert.castelo@upf.edu]>
2019-07-22 13:24:36 +0200

    Bugfix when method="ssgsea" and tau=0. See https://github.com/rcastelo/GSVA/issues/4"

--- R/gsva.R
+++ R/gsva.R
@@ -567,10 +567,10 @@ rndWalk <- function(gSetIdx, geneRanking, j, R, alpha) {
   indicatorFunInsideGeneSet <- match(geneRanking, gSetIdx)
   indicatorFunInsideGeneSet[!is.na(indicatorFunInsideGeneSet)] <- 1
   indicatorFunInsideGeneSet[is.na(indicatorFunInsideGeneSet)] <- 0
-  stepCDFinGeneSet <- cumsum((abs(R[geneRanking, j]) * 
-                      indicatorFunInsideGeneSet)^alpha) /
-                      sum((abs(R[geneRanking, j]) *
-                      indicatorFunInsideGeneSet)^alpha)
+  stepCDFinGeneSet <- cumsum((abs(R[geneRanking, j])^alpha * 
+                      indicatorFunInsideGeneSet)) /
+                      sum((abs(R[geneRanking, j])^alpha *
+                      indicatorFunInsideGeneSet))
   stepCDFoutGeneSet <- cumsum(!indicatorFunInsideGeneSet) /
                        sum(!indicatorFunInsideGeneSet)
   walkStat <- stepCDFinGeneSet - stepCDFoutGeneSet
