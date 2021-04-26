96a815843caf2e8b497176fee8daebcea1cbf84f
pablo-rodr-bio2 <pablo.rodriguez.bio2@gmail.com>
2021-04-26 12:56:26 +0200

    modularizing and fixing bugs

--- inst/shinyApp/tests/shinytest/mytest1.R
+++
@@ -1,9 +0,0 @@
-app <- ShinyDriver$new("../../", loadTimeout = 1e+05)
-app$snapshotInit("mytest1")
-
-app$uploadFile(`matrix1-matrixFile` = "matrix.csv")
-app$uploadFile(`genes1-genesetFile` = "genes.csv")
-app$setInputs(button = "click")
-app$setInputs(`modal.text-cancel` = "click")
-app$setInputs(Panels = "GeneSets")
-app$snapshot()
