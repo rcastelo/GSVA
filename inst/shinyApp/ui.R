8874e35d95dfdd68e7ecc9da4474b2638191c882
pablo-rodr-bio2 <pablo.rodriguez.bio2@gmail.com>
2021-05-06 19:51:11 +0200

    still bad buttons

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -1,21 +1,25 @@
 dashboardPage(
   title = "GSVA Shiny Application",
+  
   dashboardHeader(
     tags$li(class = "dropdown",
-            tags$style(".main-header {max-height: 75px}"),
-            tags$style(".main-header .logo {height: 75px}"),
-            tags$div("GSVA Shiny Application", style = "font-size: 30px; 
-                     color: white; font-weight: bold;")
+            tags$div(id = "app_title", "GSVA Shiny Application")
     ),
     title = tags$img(src="GSVA.png", height=75, width=75)
   ),
+  
   dashboardSidebar(
-    tags$style(".left-side, .main-sidebar {padding-top: 75px}"),
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
     ),
-    h3("Data input"),
-    #Select data source
+    fluidRow(
+      column(
+        width = 12,
+        align = "center",
+        h3("Data Input", style="font-weight: bold")
+      )
+    ),
+    # h3("Data input"),
     matrixUI("matrix1"),
     br(),
     geneSetsUI("genes1"),
@@ -23,37 +27,43 @@ dashboardPage(
     radioButtons("arg", "Change default settings:",
                  c("No" = "no",
                    "Yes" = "yes")),
-    actionButton("button", "Run"),
     br(),
-    downloadUI("download"),
-    closeBtnUI("close")
+    fluidRow(
+      actionButton("button", "Run"),
+      downloadUI("download"),
+      closeBtnUI("close"),
+    )
   ),
+  
   dashboardBody(
     shinyjs::useShinyjs(),
     add_busy_spinner(spin = "double-bounce", position = "bottom-right",
                      height = "100px", width = "100px"),
-    box(
-      width = 9,
-      tabsetPanel(id = "Panels", type="tabs",
-                  tabPanel("Samples",
-                           textOutput("errorsGsva"),
-                           htmlOutput("text1"),
-                           plot1_UI("plot1"),
-                           tableOutput("result")
-                  ),
-                  tabPanel("GeneSets",
-                           uiOutput("text2"),
-                           htmlOutput("text3"),
-                           plot2_UI("plot2"),
-                           plot3_UI("plot3")
-                  ),
-                  tabPanel("Session Info",
-                           verbatimTextOutput("sessionInfo"))
+    fluidRow(
+      box(
+        width = 9,
+        tabsetPanel(id = "Panels", type="tabs",
+                    tabPanel("Samples",
+                             textOutput("errorsGsva"),
+                             htmlOutput("text1"),
+                             plot1_UI("plot1"),
+                             tableOutput("result")
+                    ),
+                    tabPanel("GeneSets",
+                             uiOutput("text2"),
+                             htmlOutput("text3"),
+                             plot2_UI("plot2"),
+                             plot3_UI("plot3")
+                    ),
+                    tabPanel("Session Info",
+                             verbatimTextOutput("sessionInfo"))
+        )
+      ),
+      box(
+        width = 3,
+        argumentsDataUI("argumentsInput")
       )
-    ),
-    box(
-      width = 3,
-      argumentsDataUI("argumentsInput")
     )
   )
+  
 )
