aa61b1ec769618d7830ee26a1665d93a3f8baa1b
Axel Klenk <axel.klenk@gmail.com>
2023-09-21 13:57:22 +0200

    add code and docs for deprecation of old interface; add docs for new interface; eliminate argument parallel.sz; fix .gitignore

---
+++ R/GSVA-pkg-deprecated.R
@@ -0,0 +1,9 @@
+## GSVA-pkg-deprecated.R
+#' @title Deprecated functions in package `GSVA`.
+#' @description The functions listed below are deprecated and will be defunct in
+#'   the near future. When possible, alternative functions with similar
+#'   functionality are also mentioned. Help pages for deprecated functions are
+#'   available at [`gsva-deprecated`].
+#' @name GSVA-pkg-deprecated
+#' @keywords internal
+NULL
