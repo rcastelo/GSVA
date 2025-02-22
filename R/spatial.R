94f55f63f3e298946b151a893f2a0b0ea8f1d81b
peremoles <peremoles@github.com>
2025-02-22 11:42:19 +0100

    Fix small mistake when choosing first assay name as default

--- R/spatial.R
+++ R/spatial.R
@@ -28,7 +28,7 @@
 
 setMethod("spatCor", signature("SpatialExperiment"),
           function(spe, assay = NA_character_, na.rm = FALSE, alternative = "two.sided", squared = TRUE, verbose = TRUE, BPPARAM = SerialParam(progressbar = verbose)) {
-	    if(is.na(assay)) assay <- na.omit(assayNames(spe))
+	    if(is.na(assay)) assay <- na.omit(assayNames(spe))[1]
             weight_list <- .spe_dist_weight_matrix(spe,squared)
             rowns <- rownames(spe)
 	    df_res <- data.frame(observed = numeric(), expected = numeric(), sd = numeric(), p.value = numeric(), sample_id = character())
