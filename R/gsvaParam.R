b905a4b42069c773654a1dfea51f5de0e32d34b2
Axel Klenk <axel.klenk@gmail.com>
2025-03-11 19:49:04 +0100

    fix current and add extensive checking of assay names and assay argument

--- R/gsvaParam.R
+++ R/gsvaParam.R
@@ -150,14 +150,8 @@ gsvaParam <- function(exprData, geneSets,
     checkNA <- match.arg(checkNA)
     use <- match.arg(use)
 
-    an <- gsvaAssayNames(exprData)
-    if((!is.na(assay)) && (!.isCharNonEmpty(an))) {
-        msg <- sprintf(paste0("argument assay='%s' ignored since exprData has ",
-                              "no assayNames()"), assay)
-        cli_alert_info(msg)
-    }
-    if(is.na(assay) && .isCharNonEmpty(an))
-        assay <- na.omit(an)[1]
+    ## check assay parameter and assay names
+    assay <- .check_assayNames(assay, exprData)
 
     ## check for presence of valid row/feature names
     .check_rownames(exprData)
