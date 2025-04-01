f54e25e8c4195005847417a32af3259314251239
Axel Klenk <axel.klenk@gmail.com>
2025-04-01 09:36:39 +0200

    Revert "first attempt; to be tested more thoroughly"
    This reverts commit 6cd5b3d9c0c71964f7a861056222c276e725a35b.

--- R/gsva.R
+++ R/gsva.R
@@ -1,3 +1,65 @@
+##
+## function: gsva
+## purpose: main function of the package which estimates activity
+##          scores for each given gene-set
+
+#' @title Gene Set Variation Analysis
+#' @description This is the old manual page of the defunct version
+#' of the function `gsva()`.
+#'
+#' @name gsva-defunct
+## #' @usage gsva(expr, gset.idx.list, ...)
+#' @seealso [`GSVA-pkg-defunct`]
+#' @keywords internal
+NULL
+
+#' @rdname GSVA-pkg-defunct
+#' @section Details:
+#' Instead of `gsva(expr=., gset.idx.list=., method=., ...)`, use a
+#' method-specific parameter object,
+#' see [`plageParam`] [`zscoreParam`] [`ssgseaParam`] [`gsvaParam`],
+#' followed by a call to the new `gsva()` function, see [`gsva`].
+#' @export
+setMethod("gsva", signature(param="missing"), function(param, ...) {
+    .Defunct(new = "gsva()", package="GSVA",
+             msg="Calling gsva(expr=., gset.idx.list=., method=., ...) is defunct; use a method-specific parameter object (see '?gsva').")
+})
+
+#' @rdname GSVA-pkg-defunct
+#' @export
+setMethod("gsva", signature(param="matrix"), function(param, ...) {
+    .Defunct(new = "gsva()", package="GSVA",
+             msg="Calling gsva(expr=., gset.idx.list=., method=., ...) is defunct; use a method-specific parameter object (see '?gsva').")
+})
+
+#' @rdname GSVA-pkg-defunct
+#' @export
+setMethod("gsva", signature(param="ExpressionSet"), function(param, ...) {
+    .Defunct(new = "gsva()", package="GSVA",
+             msg="Calling gsva(expr=., gset.idx.list=., method=., ...) is defunct; use a method-specific parameter object (see '?gsva').")
+})
+
+#' @rdname GSVA-pkg-defunct
+#' @export
+setMethod("gsva", signature(param="SummarizedExperiment"), function(param, ...) {
+    .Defunct(new = "gsva()", package="GSVA",
+             msg="Calling gsva(expr=., gset.idx.list=., method=., ...) is defunct; use a method-specific parameter object (see '?gsva').")
+})
+
+#' @rdname GSVA-pkg-defunct
+#' @export
+setMethod("gsva", signature(param="dgCMatrix"), function(param, ...) {
+    .Defunct(new = "gsva()", package="GSVA",
+             msg="Calling gsva(expr=., gset.idx.list=., method=., ...) is defunct; use a method-specific parameter object (see '?gsva').")
+})
+
+#' @rdname GSVA-pkg-defunct
+#' @export
+setMethod("gsva", signature(param="SingleCellExperiment"), function(param, ...) {
+    .Defunct(new = "gsva()", package="GSVA",
+             msg="Calling gsva(expr=., gset.idx.list=., method=., ...) is defunct; use a method-specific parameter object (see '?gsva').")
+})
+
 
 compute.gene.cdf <- function(expr, sample.idxs, Gaussk=TRUE, kernel=TRUE,
                              sparse=FALSE, any_na=FALSE,
@@ -61,6 +123,39 @@ compute.gene.cdf <- function(expr, sample.idxs, Gaussk=TRUE, kernel=TRUE,
     return(gene.cdf)	
 }
 
+zorder_rankstat <- function(z, p) {
+  ## calculation of the ranks by expression-level statistic
+  zord <- apply(z, 2, order, decreasing=TRUE)
+
+  ## calculation of the rank-order statistic
+  zrs <- apply(zord, 2, function(x, p)
+               do.call("[<-", list(rep(0, p), x, abs(p:1-p/2))), p)
+
+  list(Zorder=zord, ZrankStat=zrs)
+}
+
+## here gSetIdx contains the positions in the decreasing gene ranking
+## and rankStat contains the rank statistic value in the original
+## gene order of the data
+.gsvaRndWalk_rankingpos_notau <- function(gSetIdx, geneRanking, rankStat) {
+    n <- length(geneRanking)
+    k <- length(gSetIdx)
+
+    stepCDFinGeneSet <- integer(n)
+    stepCDFinGeneSet[gSetIdx] <- rankStat[geneRanking[gSetIdx]]
+    stepCDFinGeneSet <- cumsum(stepCDFinGeneSet)
+    stepCDFinGeneSet <- stepCDFinGeneSet / stepCDFinGeneSet[n]
+
+    stepCDFoutGeneSet <- rep(1L, n)
+    stepCDFoutGeneSet[gSetIdx] <- 0L
+    stepCDFoutGeneSet <- cumsum(stepCDFoutGeneSet)
+    stepCDFoutGeneSet <- stepCDFoutGeneSet / stepCDFoutGeneSet[n]
+
+    walkStat <- stepCDFinGeneSet - stepCDFoutGeneSet
+
+    walkStat
+}
+
 #' @importFrom Matrix nnzero
 .sufficient_ssize <- function(expr, kcdf.min.ssize) {
   ## in the sparse case stored in a 'dgCMatrix', by now,
@@ -115,6 +210,106 @@ compute.gene.cdf <- function(expr, sample.idxs, Gaussk=TRUE, kernel=TRUE,
     }
 
     list(kernel=kernel, Gaussk=Gaussk)
+}
+
+#' @importFrom parallel splitIndices
+#' @importFrom IRanges IntegerList match
+#' @importFrom BiocParallel bpnworkers
+#' @importFrom cli cli_alert_info cli_progress_bar
+#' @importFrom cli cli_progress_update cli_progress_done
+compute.geneset.es <- function(expr, gset.idx.list, sample.idxs, kcdf,
+                               kcdf.min.ssize, abs.ranking,
+                               mx.diff=TRUE, tau=1, sparse=FALSE,
+                               verbose=TRUE, BPPARAM=SerialParam(progressbar=verbose)) {
+
+    kcdfparam <- .parse_kcdf_param(expr, kcdf, kcdf.min.ssize, sparse, verbose)
+    kernel <- kcdfparam$kernel
+    Gaussk <- kcdfparam$Gaussk
+
+    ## open parallelism only if ECDFs have to be estimated for
+    ## more than 100 genes on more than 100 samples
+    if (bpnworkers(BPPARAM) > 1 && length(sample.idxs) > 100 &&
+        nrow(expr) > 100) {
+        iter <- function(Y, idpb, n_chunks=bpnworkers(BPPARAM)) {
+            idx <- splitIndices(nrow(Y), min(nrow(Y), n_chunks))
+            i <- 0L
+            function() {
+                if (i == length(idx))
+                    return(NULL)
+                i <<- i + 1L
+                if (!is.null(idpb))
+                    cli_progress_update(id=idpb, set=i)
+                Y[idx[[i]], , drop=FALSE]
+            }
+        }
+        if (verbose) {
+            msg <- sprintf("Estimating ECDFs with %d cores",
+                           as.integer(bpnworkers(BPPARAM)))
+            idpb <- cli_progress_bar(msg, total=100)
+        }
+        gene.density <- bpiterate(iter(expr, idpb, 100),
+                                  compute.gene.cdf,
+                                  sample.idxs=sample.idxs,
+                                  Gaussk=Gaussk, kernel=kernel,
+                                  sparse=sparse, any_na=FALSE,
+                                  na_use="everything", verbose=FALSE,
+                                  REDUCE=rbind, reduce.in.order=TRUE,
+                                  BPPARAM=BPPARAM)
+        if (verbose)
+            cli_progress_done(idpb)
+    } else
+        gene.density <- compute.gene.cdf(expr, sample.idxs, Gaussk, kernel,
+                                         sparse, any_na=FALSE,
+                                         na_use="everything", verbose)
+    
+    gset.idx.list <- IntegerList(gset.idx.list)
+    n <- ncol(expr)
+    es <- NULL
+    if (n > 10 && bpnworkers(BPPARAM) > 1) {
+        if (verbose) {
+            msg <- sprintf("Calculating GSVA scores with %d cores",
+                           as.integer(bpnworkers(BPPARAM)))
+            cli_alert_info(msg)
+        }
+        es <- bplapply(as.list(1:n), function(j, Z) {
+            gene_ord_rnkstat <- list()
+            if (is(Z, "dgCMatrix"))
+                gene_ord_rnkstat <- .order_rankstat_sparse_to_sparse(Z, j)
+            else
+                gene_ord_rnkstat <- .order_rankstat(Z[, j])
+            geneRanking <- gene_ord_rnkstat[[1]]
+            rankStat <- gene_ord_rnkstat[[2]]
+
+            geneSetsRankIdx <- match(gset.idx.list, geneRanking)
+            .gsva_score_genesets(as.list(geneSetsRankIdx), geneRanking, rankStat,
+                                 mx.diff, abs.ranking, tau)
+        }, Z=gene.density, BPPARAM=BPPARAM)
+    } else {
+        idpb <- NULL
+        if (verbose)
+            idpb <- cli_progress_bar("Calculating GSVA scores", total=n)
+        es <- lapply(as.list(1:n), function(j, Z) {
+            gene_ord_rnkstat <- list()
+            if (is(Z, "dgCMatrix"))
+                gene_ord_rnkstat <- .order_rankstat_sparse_to_sparse(Z, j)
+            else
+                gene_ord_rnkstat <- .order_rankstat(Z[, j])
+            geneRanking <- gene_ord_rnkstat[[1]]
+            rankStat <- gene_ord_rnkstat[[2]]
+
+            geneSetsRankIdx <- match(gset.idx.list, geneRanking)
+            sco <- .gsva_score_genesets(as.list(geneSetsRankIdx), geneRanking, rankStat,
+                                        mx.diff, abs.ranking, tau)
+            if (verbose)
+                cli_progress_update(id=idpb)
+            sco
+        }, Z=gene.density)
+        if (verbose)
+            cli_progress_done(idpb)
+    }
+    es <- do.call("cbind", es)
+
+    return(es)
 }
 
 ##
@@ -254,6 +449,63 @@ setMethod("gsvaRanks", signature(param="gsvaParam"),
 
               return(rval)
           })
+
+.check_geneSets_minSize_maxSize_tau <- function(geneSets, minSize, maxSize, tau) {
+  if (all(!is.na(geneSets))) {
+      if (!is.list(geneSets) && !is(geneSets, "GeneSetCollection"))
+          cli_abort(c("x"="'geneSets' must be either a list or a 'GeneSetCollection' object"))
+
+      if (length(geneSets) == 0)
+          cli_abort(c("x"="'geneSets' has length 0"))
+  }
+
+  if (length(minSize) != 1)
+      cli_abort(c("x"="'minSize' must be of length 1"))
+  if (length(maxSize) != 1)
+      cli_abort(c("x"="'maxSize' must be of length 1"))
+
+  if ((is.na(minSize) && !is.na(maxSize)) || ## here assuming length 'minSize' and 'maxSize'
+      (!is.na(minSize) && is.na(maxSize)))   ## is 1, otherwise 'is.na()' would return > 1 value
+      cli_abort(c("x"="'minSize' and 'maxSize' should be either both NA or both non-NA"))
+
+  if (!is.na(minSize) && !is.na(maxSize)) {
+      if (!is.integer(minSize) && !is.numeric(minSize))
+          cli_abort(c("x"="'minSize' must be a positive integer value"))
+      if (!is.integer(maxSize) && !is.numeric(maxSize))
+          cli_abort(c("x"="'maxSize' must be a positive integer value"))
+      if (minSize < 1)
+          cli_abort(c("x"="'minSize' must be a positive integer value"))
+      if (maxSize < 1)
+          cli_abort(c("x"="'maxSize' must be a positive integer value"))
+      if (maxSize < minSize)
+          cli_abort(c("x"="'maxSize' must be at least 'minSize' or greater"))
+  }
+
+  if (length(tau) != 1)
+      cli_abort(c("x"="'tau' must be of length 1"))
+  if (!is.na(tau)) {
+    if (!is.integer(tau) && !is.numeric(tau))
+          cli_abort(c("x"="'tau' must be a numeric value"))
+  }
+}
+
+.check_maxDiff_absRanking <- function(maxDiff, absRanking) {
+  if (length(maxDiff) != 1)
+      cli_abort(c("x"="'maxDiff' must be of length 1"))
+
+  if (!is.na(maxDiff)) {
+    if (!is.logical(maxDiff))
+          cli_abort(c("x"="'maxDiff' must be a logical value"))
+  }
+
+  if (length(absRanking) != 1)
+      cli_abort(c("x"="'absRanking' must be of length 1"))
+
+  if (!is.na(absRanking)) {
+    if (!is.logical(absRanking))
+          cli_abort(c("x"="'absRanking' must be a logical value"))
+  }
+}
 
 
 #' @param param A parameter object of the [`gsvaRanksParam-class`] class.
@@ -1014,17 +1266,17 @@ setMethod("gsvaEnrichment", signature(param="gsvaRanksParam"),
   .Call("kcdfvals_sparse_to_dense_R", X, Xrsp, Gaussk, verbose)
 }
 
-## .order_rankstat <- function(x) {
-##   stopifnot(is.numeric(x)) ## QC
-##   .Call("order_rankstat_R", x)
-## }
+.order_rankstat <- function(x) {
+  stopifnot(is.numeric(x)) ## QC
+  .Call("order_rankstat_R", x)
+}
 
-## .gsva_rnd_walk <- function(gsetIdx, decOrdStat, symRnkStat) {
-##   stopifnot(is.integer(gsetIdx)) ## QC
-##   stopifnot(is.integer(decOrdStat)) ## QC
-##   stopifnot(is.numeric(symRnkStat)) ## QC
-##   .Call("gsva_rnd_walk_R", gsetIdx, decOrdStat, symRnkStat)
-## }
+.gsva_rnd_walk <- function(gsetIdx, decOrdStat, symRnkStat) {
+  stopifnot(is.integer(gsetIdx)) ## QC
+  stopifnot(is.integer(decOrdStat)) ## QC
+  stopifnot(is.numeric(symRnkStat)) ## QC
+  .Call("gsva_rnd_walk_R", gsetIdx, decOrdStat, symRnkStat)
+}
 
 #' @importFrom cli cli_abort
 .gsva_score_genesets <- function(geneSetsIdx, decOrdStat, symRnkStat,
@@ -1068,3 +1320,63 @@ setMethod("gsvaEnrichment", signature(param="gsvaRanksParam"),
 }
 
 
+
+##
+## we may not need anymore these two functions below
+##
+
+ks_test_m <- function(gset_idxs, gene.density, sort.idxs, mx.diff=TRUE,
+                      abs.ranking=FALSE, tau=1, verbose=TRUE){
+	
+	n.genes <- nrow(gene.density)
+	n.samples <- ncol(gene.density)
+	n.geneset <- length(gset_idxs)
+
+	geneset.sample.es <- .Call("ks_matrix_R",
+			                       as.double(gene.density),
+			                       as.integer(sort.idxs),
+			                       n.genes,
+			                       as.integer(gset_idxs),
+			                       n.geneset,
+			                       as.double(tau),
+			                       n.samples,
+			                       as.integer(mx.diff),
+                             as.integer(abs.ranking))
+
+	return(geneset.sample.es)
+}
+
+
+## ks-test in R code - testing only
+ks_test_Rcode <- function(gene.density, gset_idxs, tau=1, make.plot=FALSE){
+	
+	n.genes = length(gene.density)
+	n.gset = length(gset_idxs)
+	
+	sum.gset <- sum(abs(gene.density[gset_idxs])^tau)
+	
+	dec = 1 / (n.genes - n.gset)
+	
+	sort.idxs <- order(gene.density,decreasing=T)
+	offsets <- sort(match(gset_idxs, sort.idxs))
+	
+	last.idx = 0
+	values <- rep(NaN, length(gset_idxs))
+	current = 0
+	for(i in seq_along(offsets)){
+		current = current + abs(gene.density[sort.idxs[offsets[i]]])^tau / sum.gset - dec * (offsets[i]-last.idx-1)
+		
+		values[i] = current
+		last.idx = offsets[i]
+	}
+	check_zero = current - dec * (n.genes-last.idx)
+	#if(check_zero > 10^-15){ 
+	#	stop(paste=c("Expected zero sum for ks:", check_zero))
+	#}
+	if(make.plot){ plot(offsets, values,type="l") } 
+	
+	max.idx = order(abs(values),decreasing=T)[1]
+	mx.value <- values[max.idx]
+	
+	return (mx.value)
+}
