00db6fd258de6c5a2d6ad67250d9ac40d5fd0ad9
Robert Castelo <robert.castelo@upf.edu>
2026-09-20 16:55:43 +0200

    Updated .filterGenes() to move row values CLR compliance after constant value filtering

--- R/genesets.R
+++ R/genesets.R
@@ -1076,29 +1076,12 @@ setMethod("filterGeneSets", signature(gSets="GeneSetCollection"),
     rowrngs <- .processMatrixRows(expr, .rowNzRanges, anyna=anyna,
                                   verbose=verbose, BPPARAM=BPPARAM, maxmem=maxmem)
 
-    if (!is.na(rowNorm) && rowNorm == "clr") {
-        whcol <- 1
-        if (ncol(rowrngs) <= 2) { ## dense input
-            if (any(rowrngs[, 1] <= 0)) {
-                msg <- paste("Cannot apply row normalization method 'clr' to",
-                             "expression data with nonpositive values")
-                cli_abort(c("x"=msg))
-            }
-        } else { ## sparse input
-            if (any(rowrngs[, 3] <= 0)) {
-                msg <- paste("Cannot apply row normalization method 'clr' to",
-                             "expression data with nonzero nonpositive values")
-                cli_abort(c("x"=msg))
-            }
-        }
-    }
-
     constantRows <- (rowrngs[, 1] == rowrngs[, 2])
     mask <- is.na(constantRows)
     if (any(mask))
         constantRows[mask] <- TRUE
 
-    constantNzRows <- invalidRows <- invalidNzRows <- rep(FALSE, nrow(expr))
+    constantNzRows <- rep(FALSE, nrow(expr))
     if (ncol(rowrngs) > 2) { ## sparse input
         constantNzRows <- (rowrngs[, 3] == rowrngs[, 4])
         mask <- is.na(constantNzRows)
@@ -1130,17 +1113,34 @@ setMethod("filterGeneSets", signature(gSets="GeneSetCollection"),
     if (removeNzConstant && any(nzmask))
          removemask <- removemask | nzmask
 
-    if (any(removemask)) {
-        if (nrow(expr) - sum(removemask) < 2) {
-            msg <- "Less than two rows left in the input assay object."
-            if (errorOnTooFewRows)
-                cli_abort(c("x"=msg))
-            else
-                cli_alert_warning(msg)
-        }
-
-        expr <- expr[!removemask, , drop=FALSE]
+    if (any(removemask) && (nrow(expr) - sum(removemask)) < 2) {
+        msg <- "Less than two rows left in the input assay object."
+        if (errorOnTooFewRows)
+            cli_abort(c("x"=msg))
+        else
+            cli_alert_warning(msg)
     }
+
+    ## check for CLR compliant input data in nonconstant rows only
+    if (!is.na(rowNorm) && rowNorm == "clr") {
+        rowrngs <- rowrngs[!removemask, , drop=FALSE]
+        if (ncol(rowrngs) <= 2) { ## dense input
+            if (any(rowrngs[, 1] <= 0)) {
+                msg <- paste("Cannot apply row normalization method 'clr' to",
+                             "expression data with nonpositive values")
+                cli_abort(c("x"=msg))
+            }
+        } else { ## sparse input
+            if (any(rowrngs[, 3] <= 0)) {
+                msg <- paste("Cannot apply row normalization method 'clr' to",
+                             "expression data with nonzero nonpositive values")
+                cli_abort(c("x"=msg))
+            }
+        }
+    }
+
+    if (any(removemask))
+        expr <- expr[!removemask, , drop=FALSE]
 
     return(expr)
 }
